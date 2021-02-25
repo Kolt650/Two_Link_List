cd4244706652ee7ee8da679d3763943282ff7309
Kolt <ilya.kolt@gmail.com>
2021-02-25 15:13:31 +0300

    first commit

---
+++ GeekBrains.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Two_Link_List
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+    }
+
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+       // Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+    public class Realize : ILinkedList
+    {
+        Node head = new Node();
+        Node tail = new Node();
+
+        //Добавленый мной метод
+        public void Inicilization()
+        {
+            head = null;
+            tail = null;
+        }
+
+        // возвращает количество элементов в списке
+        public int GetCount()
+        {
+            int count = 0;
+            if (head == null)
+                return count;
+            else
+            {
+                var list = new Node();
+                list = head;
+                while (list != null)
+                {
+                    count++;
+                    Console.WriteLine(count+" Элемент "+" равен "+list.Value);
+                    list = list.NextNode;
+                }
+                return count;
+            }
+        }
+
+        // добавляет новый элемент списка
+        public void AddNode(int value)
+        {
+
+
+            var newlist = new Node();
+            newlist.Value = value;
+
+            if (head == null)
+                head = newlist;
+            else
+            {
+                var list = head;
+                while (list.NextNode != null)
+                    list = list.NextNode;
+
+                list.NextNode = newlist;
+                newlist.PrevNode = list;
+            }
+            tail = newlist;
+        }
+
+        // добавляет новый элемент списка после определённого элемента
+        public void AddNodeAfter(Node node, int value)
+        {
+            if (head == null)
+                head = node;
+            else 
+            {
+                var list = new Node();
+                list = head;
+                while ((list.Value != value) && (list.NextNode != null))
+                    list = list.NextNode;
+                if (list != null)
+                {
+                    node.PrevNode = list;
+                    node.NextNode = list.NextNode;
+                    list.NextNode = node;
+                }
+                else
+                    Console.WriteLine("Данное значение отсутствует в списке");
+            }
+            tail = tail.NextNode;
+        }
+
+        // удаляет элемент по порядковому номеру
+        //Поставь обратный порядок ифов с элсами!!!
+        public void RemoveNode(int index)
+        {
+            if (index == 1)
+            {
+                head = head.NextNode;
+                head.PrevNode = null;
+            }
+            else
+            {
+                var list = head;
+                int count = 0;
+                while ((count != index - 1) && (list.NextNode != null))
+                {
+                    list = list.NextNode;
+                    count++;
+                }
+                if (list.NextNode != null)
+                {
+                    list.PrevNode.NextNode = list.NextNode;
+                    list.NextNode.PrevNode = list.PrevNode;
+                    list = null;
+                }
+                else if (list.NextNode == null)
+                {
+                    if (count < index)
+                        Console.WriteLine("Ошибка ввода индекса, не найдено");
+                    else
+                    {
+                        list.PrevNode.NextNode = null;
+                        tail = list;
+                    }
+                }
+            }
+        }
+
+        // удаляет указанный элемент
+        public void RemoveNode(Node node)
+        {
+            if (node == head)
+            {
+                head = head.NextNode;
+                head.PrevNode = null;
+            }
+            else
+            {
+                var list = head;
+                int count = 0;
+                while ((list != node) && (list.NextNode != null))
+                {
+                    list = list.NextNode;
+                    count++;
+                }
+                if (list.NextNode != null)
+                {
+                    list.PrevNode.NextNode = list.NextNode;
+                    list.NextNode.PrevNode = list.PrevNode;
+                    list = null;
+                }
+                else if (list.NextNode == null)
+                {
+                    if ( list != node )
+                        Console.WriteLine("Ошибка ввода индекса , не найдено");
+                    else
+                    {
+                        list.PrevNode.NextNode = null;
+                        tail = list;
+                    }
+                }
+            }
+        }
+
+         //ищет элемент по его значению
+        public Node FindNode(int searchValue)
+        {
+            var list = head;
+            while (list.Value != searchValue)
+                list=list.NextNode;
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Требуется реализовать класс двусвязного списка и операции вставки, удаления и поиска элемента в нём в соответствии с интерфейсом.
+
+namespace Two_Link_List
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Создание экземпляра класса список
+            var Node = new Realize();
+            Node.Inicilization(); 
+            //Инициализация, присвоение начального значения
+            //0
+
+            //Заполнения списка
+            for (int i = 1; i <= 5; i++)
+                Node.AddNode(i);
+            //ожидается добавление элемента i
+            //1 2 3 4 5
+
+            //Создание элемента списка, присваивание ему значения и добавление в список
+            var new_list = new Node();
+            new_list.Value = 6;
+            Node.AddNodeAfter(new_list,5);
+            //ожидается добавление элемента 6, после элемента 5
+            //1 2 3 4 5 6
+
+            //удаление элемента по его индексу
+            Node.RemoveNode(5);
+            //ожидается удаление элемента 5
+            //1 2 3 4 6
+
+            var new_new_list = new Node();
+            new_new_list.Value = 10;
+
+            //удаление элемента 
+            Node.RemoveNode(new_new_list);
+            
+            // ожидается, ошибка содержимого элемента
+            //1 2 3 4 6
+
+            Node.RemoveNode(new_list);
+            //ожидается удаление элемента 6
+            //1 2 3 4 
+
+            new_list=Node.FindNode(4);
+            Console.WriteLine("найденый элемент " + new_list.Value);
+            //ожидается 4
+            //1 2 3 4 
+
+            //Вывод списка и подсчёт количества его элемента
+            Console.WriteLine("Всего элементов " + Node.GetCount());
+            // ожидается 1 2 3 4 
+            Console.ReadKey();
+        }
+    }
+
+}
